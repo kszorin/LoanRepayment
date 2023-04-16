45bdeb022c47cd912bec606d1a0a957d2e8c687a
kszorin <kszorin@yandex.ru>
2023-04-16 17:03:02 +0700

    LR-3 Поправил запуск CalculatePaymentUsecaseTest
    Использую runTest вместо runBlocking.

--- loan/api/src/test/java/ru/kszorin/loanrepayment/loan/api/domain/usecase/CalculatePaymentUsecaseTest.kt
+++ loan/api/src/test/java/ru/kszorin/loanrepayment/loan/api/domain/usecase/CalculatePaymentUsecaseTest.kt
@@ -1,6 +1,7 @@
 package ru.kszorin.loanrepayment.loan.api.domain.usecase
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -8,20 +9,21 @@ import org.mockito.junit.MockitoJUnitRunner
 import ru.kszorin.loanrepayment.loan.api.domain.model.Payment
 import kotlin.math.pow
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
 internal class CalculatePaymentUsecaseTest {
 
 	private val usecase = CalculatePaymentsUsecase()
 
 	@Test
-	fun `WHEN calculate payment EXPECT loan payment`() = runBlocking {
+	fun `WHEN calculate payment EXPECT loan payment`() = runTest {
 		val s = 200000.0
 		val rate = 12.0
 		val p = rate / 100 / 12
 		val n = 24
 		val x = s * (p + p / ((1 + p).pow(n) - 1))
 		val xStr = "%.2f".format(x)
-		println("Expeted:\ns = $s\np = $p\nn=$n\nx=$xStr")
+		println("Expected:\ns = $s\np = $p\nn=$n\nx=$xStr")
 
 		val actual = usecase(
 			sum = 200000.0,
